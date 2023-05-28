17ce4da5a74435fa149a5bcca662bf24e046adfa
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-05-29 00:15:25 +0300

    (

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 const API = "live_XTVqmtwZCRUux0xyyuBoz9aUlTLhAmYM8XZI7lwTTnN9wNP285Bwa4shn7cLcow8";
-const CAT_SEARCH = 'https://api.thecatapi.com/v1/images';
+const CAT_SEARCH = 'https://api.thecatapi.com/v1/images/search';
 
 export function fetchBreeds(keyword){
       const params = new URLSearchParams({
@@ -13,7 +13,7 @@ export function fetchBreeds(keyword){
             if (!r.ok) {
             throw new error(r.status)
         }
-            return r.json
+            return r.json()
         })
     .catch((error) => console.log(error));
 };
@@ -25,7 +25,7 @@ export function fetchCatByBreed(breedId) {
             if (!r.ok) {
             throw new error(r.status)
         }
-            return r.json
+            return r.json()
         })
     .catch((error) => console.log(error));
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-import {fetchCatByBreed} from './cat-api.js'
-import {fetchBreeds} from './cat-api.js'
+import { fetchCatByBreed, fetchBreeds } from './cat-api.js'
+import Notiflix from 'notiflix';
+import 'slim-select/dist/slimselect.css'
 import SlimSelect from 'slim-select'
-
-// new SlimSelect({
-// select: '#selectElement'
-// })
-
+        
 const catInfo = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+
+loader.classList.add('unvisible')
 
 function getBreeds(data) {
     fetchBreeds(data).then(data => data);
 };
-getBreeds('objects');
+getBreeds('objectBreeds');
+
+function updateSelect(data) {
+    fetchBreeds(data).then(data => {
+        console.log(data)
+        const markupBreeds = data.map(({ id, name }) => {
+            return `<li><option value ='${id}'>${name}</option></li>`;
+        }).join('');
+        select.insertAdjacentHTML('beforeend', markupBreeds);
+        // new SlimSelect({
+        //     select: '#single'
+        // });
+
+    });
+};
+updateSelect();
 
 select.addEventListener("change", onSelected);
 
@@ -22,20 +37,15 @@ function onSelected(e) {
     fetchCatByBreed(breedId).then((data) => {
         console.log(data)
         const markupCats = data
-        .map(({ name, description, reference_image_id, temperament}) => {
-            return `<li><h1>${name}</h1><img src=${reference_image_id} alt='${name}' width='200'><p>${temperament}</p><p>${description}</p></li>`;
+        .map(({name,url,description,temperament}) => {
+            return `<div><h1>${name}</h1>
+            <img src=${url} alt='${name}' width='400'>
+            <p>${temperament}</p>
+            <p>${description}</p></div>`;
         }).join('');
-    catInfo.insertAdjacentHTML('beforeend', markupCats);
-  });
+        catInfo.insertAdjacentHTML('beforeend', markupCats)
+        // .catch(error => console.log(error))
+        // Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    })
+    // .finaly(()=>loader.classList.add('unvisible'))
 };
-
-function updateSelect(data) {
-    fetchBreeds(data).then(data => {
-        console.log(data)
-        const markupBreeds = data.map(({ name, id }) => {
-            return `<li><option value ='${id}'>${name}</option></li>`;
-        }).join('');
-    select.insertAdjacentHTML('beforeend', markupBreeds);
-  });
-};
-updateSelect('markup');
