bb12212c69dc996a9f73d00cfbb7fda65f8a8f5d
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-05-29 19:54:12 +0300

    pz

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 const API = "live_XTVqmtwZCRUux0xyyuBoz9aUlTLhAmYM8XZI7lwTTnN9wNP285Bwa4shn7cLcow8";
 const CAT_SEARCH = 'https://api.thecatapi.com/v1/images/search';
@@ -8,24 +10,22 @@ export function fetchBreeds(keyword){
           keyword
     });
     return fetch(`${BASE_URL}?${params}`)
-        .then((r) =>
-        { 
-            if (!r.ok) {
+    .then((r) => {
+        if (!r.ok) {
             throw new error(r.status)
         }
-            return r.json()
-        })
-    .catch((error) => console.log(error));
+        return r.json()
+    })
+    .catch((error) => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!'));
 };
 
 export function fetchCatByBreed(breedId) {
     return fetch(`${CAT_SEARCH}?breed_ids=${breedId}&apikey=${API}`)
-        .then((r) =>
-        { 
-            if (!r.ok) {
+    .then((r) => {
+        if (!r.ok) {
             throw new error(r.status)
         }
-            return r.json()
-        })
-    .catch((error) => console.log(error));
+        return r.json()
+    })
+    .catch((error) => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!'));
 };
--- src/index.js
+++ src/index.js
@@ -1,51 +1,74 @@
 import { fetchCatByBreed, fetchBreeds } from './cat-api.js'
-import Notiflix from 'notiflix';
-import 'slim-select/dist/slimselect.css'
 import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css'
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
         
 const catInfo = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
 
-loader.classList.add('unvisible')
+
+new SlimSelect({
+  select: '#selectElement',
+})
+
+getBreeds('objectBreeds');
 
 function getBreeds(data) {
     fetchBreeds(data).then(data => data);
 };
-getBreeds('objectBreeds');
+
+updateSelect();
 
 function updateSelect(data) {
     fetchBreeds(data).then(data => {
         console.log(data)
         const markupBreeds = data.map(({ id, name }) => {
-            return `<li><option value ='${id}'>${name}</option></li>`;
+            return 
         }).join('');
         select.insertAdjacentHTML('beforeend', markupBreeds);
-        // new SlimSelect({
-        //     select: '#single'
-        // });
-
     });
 };
-updateSelect();
 
 select.addEventListener("change", onSelected);
 
+
+
+Loading.remove('loader');
+errorEl.style.display = 'none'
+loader.style.display = 'none'
+
 function onSelected(e) {
+    Loading.dots();
     let breedId = e.target.value;
 
     fetchCatByBreed(breedId).then((data) => {
         console.log(data)
-        const markupCats = data
-        .map(({name,url,description,temperament}) => {
-            return `<div><h1>${name}</h1>
-            <img src=${url} alt='${name}' width='400'>
-            <p>${temperament}</p>
-            <p>${description}</p></div>`;
-        }).join('');
-        catInfo.insertAdjacentHTML('beforeend', markupCats)
-        // .catch(error => console.log(error))
-        // Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    const markupCats = data[0].breeds.map(({name, description, temperament})  => {
+           return `<h1>${name}</h1>
+        <p>${description}</p>
+        <p>Temperament: ${temperament}</p>`;
+       })
+       .join('');
+       const markupPicture = data
+           .map(({ url }) => {
+         return `<img src='${url}' width='600'>`
+           }
+         )
+      .join('');
+        catInfo.insertAdjacentHTML('beforeend', markupPicture);
+        catInfo.insertAdjacentHTML('beforeend', markupCats);
     })
-    // .finaly(()=>loader.classList.add('unvisible'))
+    .finally(() => {
+        Loading.remove();
+    });
+    catInfo.innerHTML = '';
 };
+
+
+
+// loader.style.display = 'block'
+// Loading.dots(loader.style.display = 'none');
+// loader.classList.add('unvisible');
+// .finaly(() => loader.classList.add('unvisible'));
